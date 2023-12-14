ef73251d86244806f24a113bf3881ffa4b6585c3
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-12-14 17:38:54 +0900

    feat: implement MinimalZKSyncAccount.validateTransaction

--- contracts/MinimalZKSyncAccount.sol
+++ contracts/MinimalZKSyncAccount.sol
@@ -1,17 +1,22 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.23;
 
-import {BOOTLOADER_FORMAL_ADDRESS} from "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
+import {BOOTLOADER_FORMAL_ADDRESS, NONCE_HOLDER_SYSTEM_CONTRACT} from "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
 import {ACCOUNT_VALIDATION_SUCCESS_MAGIC, IAccount} from "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IAccount.sol";
-import {Transaction} from "@matterlabs/zksync-contracts/l2/system-contracts/libraries/TransactionHelper.sol";
+import {INonceHolder} from "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/INonceHolder.sol";
+import {SystemContractsCaller} from "@matterlabs/zksync-contracts/l2/system-contracts/libraries/SystemContractsCaller.sol";
+import {Transaction, TransactionHelper} from "@matterlabs/zksync-contracts/l2/system-contracts/libraries/TransactionHelper.sol";
 
 import "@openzeppelin/contracts/interfaces/IERC1271.sol";
 
 import "./Constants.sol";
 
-error InvalidBootloader(address bootloader);
+error InvalidBootloader(address);
+error InsufficientBalance(uint256 available, uint256 required);
 
 contract MinimalZKSyncAccount is IAccount, IERC1271 {
+    using TransactionHelper for Transaction;
+
     modifier onlyBootloader() {
         if (msg.sender != BOOTLOADER_FORMAL_ADDRESS) {
             revert InvalidBootloader(msg.sender);
@@ -22,8 +27,22 @@ contract MinimalZKSyncAccount is IAccount, IERC1271 {
     function validateTransaction(
         bytes32,
         bytes32,
-        Transaction calldata
+        Transaction calldata tx_
     ) external payable override onlyBootloader returns (bytes4) {
+        SystemContractsCaller.systemCallWithPropagatedRevert(
+            uint32(gasleft()),
+            address(NONCE_HOLDER_SYSTEM_CONTRACT),
+            0,
+            abi.encodeCall(INonceHolder.incrementMinNonceIfEquals, (tx_.nonce))
+        );
+
+        uint256 availableBalance = address(this).balance;
+        uint256 requiredBalance = tx_.totalRequiredBalance();
+
+        if (availableBalance < requiredBalance) {
+            revert InsufficientBalance(availableBalance, requiredBalance);
+        }
+
         return ACCOUNT_VALIDATION_SUCCESS_MAGIC;
     }
 
